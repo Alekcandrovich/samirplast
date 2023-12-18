7e08591c412835efa398e38281d1769932226e2a
Alekcandrovich <ravara128@gmail.com>
2023-12-18 17:47:38 +0200

    mayka+paket+new

--- src/pages/Products/Mayka.js
+++ src/pages/Products/Mayka.js
@@ -1,61 +1,61 @@
-// import React from 'react';
-// import mayka_1 from '../../images/mayka_1.jpg';
-// import mayka_2 from '../../images/mayka_2.jpg';
+import React from 'react';
+import mayka_1 from '../../images/mayka_1.jpg';
+import mayka_2 from '../../images/mayka_2.jpg';
 
-// const Mayka = () => {
-//   return (
-//     <main>
-//     <section className='section3'>
-//       <div className='container'>
-//         <div className='product_detail_1 open-modal_product'>
-//           <img className='product_detail_image' src={mayka_1} alt='ПАКЕТИ ТИПУ - МАЙКА' />
-//           <div className='product_detail_info'>
-//             <h2>ПАКЕТИ ТИПУ - МАЙКА</h2>
-//             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Найдоступнiший за ціною варіант серед пакетів з ручками.
-//               Цей тип пакетів найчастіше використовується торгівельними i торгово-промисловими компаніями,
-//               магазинами и аптеками для упаковки покупок. Якісні характеристики i велике різноманіття за
-//               розмірами i товщиною стінок пакетів «майка» дозволили їм набути популярності, як у
-//               продавців, так i у покупців.<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Основні характеристики:<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- низька вартість<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- велика вантажопідйомність<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- висока міцність<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- місткість за рахунок бічних складок<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- зручність використання
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//     <section className='section3'>
-//       <div className='container'>
-//         <div className='product_detail_2 open-modal_product'>
-//           <img className='product_detail_image' src={mayka_2} alt='ОПИС ПАКЕТIВ ТИПУ - МАЙКА' />
-//           <div className='product_detail_info'>
-//             <h2>ОПИС ПАКЕТIВ ТИПУ - МАЙКА</h2>
-//             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Цей вид пакетів виготовляється з рукава поліетилену низького
-//               тиску. Особливістю підготовленого рукава є фальці (бічні складки), які після належної обробки
-//               формують ручки пакету.<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Всі пакети "майка" мають наступне стандартне позначення,
-//               наприклад: (28+8х2)х47/15, де:<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 28 - ширина пакету в сантиметрах<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 8 - ширина фальця (бічної складки) в сантиметрах<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 47 - повна висота пакету (від дна до краю ручки) в сантиметрах<br />
-//               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 15 - товщина стінки пакету в мікронах
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//     <section className='section4'>
-//       <div className='button'>
-//         <button type="button" className="product_button open-modal_price" data-pdf-src="../prices/mayka.pdf">
-//           ПОДИВИТИСЯ ПРАЙС - ЛИСТ
-//         </button>
-//       </div>
-//     </section>
-//   </main>
-//   );
-// };
+const Mayka = () => {
+  return (
+    <main>
+    <section className='section3'>
+      <div className='container'>
+        <div className='product_detail_1 open-modal_product'>
+          <img className='product_detail_image' src={mayka_1} alt='ПАКЕТИ ТИПУ - МАЙКА' />
+          <div className='product_detail_info'>
+            <h2>ПАКЕТИ ТИПУ - МАЙКА</h2>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Найдоступнiший за ціною варіант серед пакетів з ручками.
+              Цей тип пакетів найчастіше використовується торгівельними i торгово-промисловими компаніями,
+              магазинами и аптеками для упаковки покупок. Якісні характеристики i велике різноманіття за
+              розмірами i товщиною стінок пакетів «майка» дозволили їм набути популярності, як у
+              продавців, так i у покупців.<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Основні характеристики:<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- низька вартість<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- велика вантажопідйомність<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- висока міцність<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- місткість за рахунок бічних складок<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- зручність використання
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className='section3'>
+      <div className='container'>
+        <div className='product_detail_2 open-modal_product'>
+          <img className='product_detail_image' src={mayka_2} alt='ОПИС ПАКЕТIВ ТИПУ - МАЙКА' />
+          <div className='product_detail_info'>
+            <h2>ОПИС ПАКЕТIВ ТИПУ - МАЙКА</h2>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Цей вид пакетів виготовляється з рукава поліетилену низького
+              тиску. Особливістю підготовленого рукава є фальці (бічні складки), які після належної обробки
+              формують ручки пакету.<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Всі пакети "майка" мають наступне стандартне позначення,
+              наприклад: (28+8х2)х47/15, де:<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 28 - ширина пакету в сантиметрах<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 8 - ширина фальця (бічної складки) в сантиметрах<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 47 - повна висота пакету (від дна до краю ручки) в сантиметрах<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 15 - товщина стінки пакету в мікронах
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className='section4'>
+      <div className='button'>
+        <button type="button" className="product_button open-modal_price" data-pdf-src="../prices/mayka.pdf">
+          ПОДИВИТИСЯ ПРАЙС - ЛИСТ
+        </button>
+      </div>
+    </section>
+  </main>
+  );
+};
 
-// export default Mayka;
+export default Mayka;
