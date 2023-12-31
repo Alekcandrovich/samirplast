c0cd87379e6bef455bb72bddbfa9e700593134f2
Alekcandrovich <ravara128@gmail.com>
2023-12-31 16:36:42 +0200

    стили слайдера готовы

--- src/components/Modal/ModalProduct.js
+++ src/components/Modal/ModalProduct.js
@@ -2,7 +2,24 @@ import React, { useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import icons from './icons.svg';
 import './styles.css';
+
+const PrevArrow = ({ onClick }) => (
+  <div className="slide-arrow prev" onClick={onClick}>
+    <svg className="icon-modal">
+      <use xlinkHref={`${icons}#left`} />
+    </svg>
+  </div>
+);
+
+const NextArrow = ({ onClick }) => (
+  <div className="slide-arrow next" onClick={onClick}>
+    <svg className="icon-modal">
+      <use xlinkHref={`${icons}#right`} />
+    </svg>
+  </div>
+);
 
 const ModalProduct = ({ images, closeModal }) => {
   useEffect(() => {
@@ -27,18 +44,16 @@ const ModalProduct = ({ images, closeModal }) => {
     autoplaySpeed: 2000,
     infinite: true,
     pauseOnHover: false,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
 
   return (
     <div className="modal-overlay">
       <div className="modal-product">
-        <button
-          type="button"
-          className="close-modal"
-          onClick={closeModal}
-        >
+        <button type="button" className="close-modal" onClick={closeModal}>
           <svg className="icon-modal">
-            <use xlinkHref="./icons.svg#close"></use>
+            <use xlinkHref={`${icons}#close`} />
           </svg>
         </button>
         <Slider {...settings}>
