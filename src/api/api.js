6887318d65201048c6365f0124a9a785f1d56adc
Alekcandrovich <ravara128@gmail.com>
2024-01-16 19:55:36 +0200

    отзывы заработали, остался дизайн

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,18 @@
+// import axios from 'axios';
+
+// const API_URL = `https://65a6b61f74cf4207b4f0aee2.mockapi.io/reviews`;
+// // const API_URL = `https://64aec52dc85640541d4dabc5.mockapi.io`;
+
+// export const fetchReviewsApi = () => axios.get(`${API_URL}/reviews`);
+// export const addReviewApi = newReview =>
+//   axios.post(`${API_URL}/reviews`, newReview);
+
+
+// api.js
 import axios from 'axios';
 
-const API_URL = `https://65a2b8e842ecd7d7f0a82095.mockapi.io`;
+const API_URL = `https://65a6b61f74cf4207b4f0aee2.mockapi.io`;
 
-export const fetchContactsApi = () => axios.get(`${API_URL}/reviews`);
-export const addContactApi = newContact =>
-  axios.post(`${API_URL}/reviews`, newContact);
-export const deleteContactApi = id => axios.delete(`${API_URL}/reviews/${id}`);
+export const fetchReviewsApi = () => axios.get(`${API_URL}/reviews`);
+export const addReviewApi = newReview => axios.post(`${API_URL}/reviews`, newReview);
+
