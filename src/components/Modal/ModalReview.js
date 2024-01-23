61642eec8ee9e82cabbc4d1fec33fd6047935c60
Alekcandrovich <ravara128@gmail.com>
2024-01-23 14:46:45 +0200

    восстановил стили для модалки отзывов

--- src/components/Modal/ModalReview.js
+++ src/components/Modal/ModalReview.js
@@ -1,349 +1,8 @@
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { setReviews } from '../../redux/actions';
-// import Notiflix from 'notiflix';
-// import { addReviewApi, fetchReviewsApi } from '../../api/api';
-// import icons from './icons.svg';
-// import './styles.css';
-
-// const ModalReview = ({ closeModal, onSuccess }) => {
-//   const [newReview, setNewReview] = useState({ name: '', comment: '' });
-//   const dispatch = useDispatch();
-
-//   const handleInputChange = e => {
-//     const { name, value } = e.target;
-//     setNewReview(prevReview => ({ ...prevReview, [name]: value }));
-//   };
-
-//   const canSubmit =
-//     newReview.name.trim() !== '' && newReview.comment.trim() !== '';
-
-//   const handleSubmit = async e => {
-//     e.preventDefault();
-
-//     console.log('Submitting form...');
-
-//     console.log('Name:', newReview.name); // Добавьте эту строку для отладки
-//     console.log('Comment:', newReview.comment); // Добавьте эту строку для отладки
-
-//     if (!canSubmit) {
-//       console.log('Empty fields detected');
-//       Notiflix.Notify.failure('Пожалуйста, заполните все поля формы.'); // Отображение сообщения об ошибке
-//       return;
-//     }
-
-//     try {
-//       console.log('Adding review...');
-//       await addReviewApi(newReview);
-//       console.log('Review added successfully.');
-
-//       console.log('Fetching reviews...');
-//       const data = await fetchReviewsApi();
-//       console.log('Fetched reviews:', data);
-
-//       dispatch(setReviews(data));
-//       onSuccess();
-//       closeModal();
-//     } catch (error) {
-//       console.error('Error adding review:', error);
-//       console.error('Ошибка при добавлении отзыва:', error);
-//     }
-
-//     setNewReview({ name: '', comment: '' });
-//   };
-
-//   console.log('Rendering ModalReview...');
-
-//   return (
-//     <div className="modale_overlay" onClick={closeModal}>
-//       <div className="modale_content" onClick={e => e.stopPropagation()}>
-//         <button className="close_modal" onClick={closeModal}>
-//           <svg className="icon_modal">
-//             <use xlinkHref={`${icons}#close`} />
-//           </svg>
-//         </button>
-//         <form className="reviews_form" onSubmit={handleSubmit}>
-//           <label className="reviews_label">
-//             Ім'я:
-//             <input
-//               className="reviews_input"
-//               type="text"
-//               name="name"
-//               value={newReview.name}
-//               autoComplete="name"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           <label>
-//             Коментар:
-//             <textarea
-//               className="reviews_textarea"
-//               name="comment"
-//               value={newReview.comment}
-//               autoComplete="comment"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           <button
-//             className="reviews_button"
-//             type="submit"
-//             disabled={!canSubmit}
-//           >
-//             ВІДПРАВИТИ ВІДГУК
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ModalReview;
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { setReviews } from '../../redux/actions';
-// import { addReviewApi, fetchReviewsApi } from '../../api/api';
-// import icons from './icons.svg';
-// import './styles.css';
-
-// const ModalReview = ({ closeModal, onSuccess }) => {
-//   const [newReview, setNewReview] = useState({ name: '', comment: '' });
-//   const dispatch = useDispatch();
-
-//   const handleInputChange = e => {
-//     const { name, value } = e.target;
-//     setNewReview(prevReview => ({ ...prevReview, [name]: value }));
-//   };
-
-//   const canSubmit =
-//     newReview.name.trim() !== '' && newReview.comment.trim() !== '';
-
-//   const handleSubmit = async e => {
-//     e.preventDefault();
-
-//     console.log('Submitting form...');
-
-//     console.log('Name:', newReview.name);
-//     console.log('Comment:', newReview.comment);
-
-//     try {
-//       console.log('Adding review...');
-//       await addReviewApi(newReview);
-//       console.log('Review added successfully.');
-
-//       console.log('Fetching reviews...');
-//       const data = await fetchReviewsApi();
-//       console.log('Fetched reviews:', data);
-
-//       dispatch(setReviews(data));
-//       onSuccess();
-//       closeModal();
-//     } catch (error) {
-//       console.error('Error adding review:', error);
-//       console.error('Ошибка при добавлении отзыва:', error);
-//     }
-
-//     setNewReview({ name: '', comment: '' });
-//   };
-
-//   console.log('Rendering ModalReview...');
-
-//   return (
-//     <div className="modale_overlay" onClick={closeModal}>
-//       <div className="modale_content" onClick={e => e.stopPropagation()}>
-//         <button className="close_modal" onClick={closeModal}>
-//           <svg className="icon_modal">
-//             <use xlinkHref={`${icons}#close`} />
-//           </svg>
-//         </button>
-//         <form className="reviews_form" onSubmit={handleSubmit}>
-//           <label className="reviews_label">
-//             Ім'я:
-//             <input
-//               className="reviews_input"
-//               type="text"
-//               name="name"
-//               value={newReview.name}
-//               autoComplete="name"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           <label>
-//             Коментар:
-//             <textarea
-//               className="reviews_textarea"
-//               name="comment"
-//               value={newReview.comment}
-//               autoComplete="comment"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           <button
-//             className="reviews_button"
-//             type="submit"
-//             disabled={!canSubmit}
-//           >
-//             ВІДПРАВИТИ ВІДГУК
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ModalReview;
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { setReviews } from '../../redux/actions';
-// import { addReviewApi, fetchReviewsApi } from '../../api/api';
-// import icons from './icons.svg';
-// import './styles.css';
-
-// const ModalReview = ({ closeModal, onSuccess }) => {
-//   const [newReview, setNewReview] = useState({
-//     name: { value: '', touched: false },
-//     comment: { value: '', touched: false },
-//   });
-//   const dispatch = useDispatch();
-
-//   const handleInputChange = e => {
-//     const { name, value } = e.target;
-//     setNewReview(prevReview => ({
-//       ...prevReview,
-//       [name]: { value, touched: true },
-//     }));
-//   };
-
-//   const canSubmit =
-//     newReview.name.value.trim() !== '' &&
-//     newReview.comment.value.trim() !== '' &&
-//     newReview.name.touched &&
-//     newReview.comment.touched;
-
-//   const handleSubmit = async e => {
-//     e.preventDefault();
-
-//     console.log('Submitting form...');
-
-//     console.log('Name:', newReview.name.value);
-//     console.log('Comment:', newReview.comment.value);
-
-//     if (!canSubmit) {
-//       console.log('Form not filled out completely');
-//       return;
-//     }
-
-//     try {
-//       console.log('Adding review...');
-//       await addReviewApi(newReview);
-//       console.log('Review added successfully.');
-
-//       console.log('Fetching reviews...');
-//       const data = await fetchReviewsApi();
-//       console.log('Fetched reviews:', data);
-
-//       dispatch(setReviews(data));
-//       onSuccess();
-//       closeModal();
-//     } catch (error) {
-//       console.error('Error adding review:', error);
-//       console.error('Ошибка при добавлении отзыва:', error);
-//     } finally {
-//       setNewReview(prevReview => ({
-//         ...prevReview,
-//         name: { value: '', touched: false },
-//         comment: { value: '', touched: false },
-//       }));
-//     }
-//   };
-
-//   console.log('Rendering ModalReview...');
-
-//   return (
-//     <div className="modale_overlay" onClick={closeModal}>
-//       <div className="modale_content" onClick={e => e.stopPropagation()}>
-//         <button className="close_modal" onClick={closeModal}>
-//           <svg className="icon_modal">
-//             <use xlinkHref={`${icons}#close`} />
-//           </svg>
-//         </button>
-//         <form className="reviews_form" onSubmit={handleSubmit}>
-//           <label className="reviews_label">
-//             Ім'я:
-//             <input
-//               className="reviews_input"
-//               type="text"
-//               name="name"
-//               value={
-//                 newReview.name && typeof newReview.name === 'object'
-//                   ? newReview.name.value
-//                   : newReview.name
-//               }
-//               autoComplete="name"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           <label>
-//             Коментар:
-//             <textarea
-//               className="reviews_textarea"
-//               name="comment"
-//               value={
-//                 newReview.comment && typeof newReview.comment === 'object'
-//                   ? newReview.comment.value
-//                   : newReview.comment
-//               }
-//               autoComplete="comment"
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//           <br />
-//           {!canSubmit && (
-//             <div className="error-message">
-//               <p>Пожалуйста, заполните все поля формы</p>
-//             </div>
-//           )}
-//           <button
-//             className="reviews_button"
-//             type="submit"
-//             disabled={!canSubmit}
-//           >
-//             ВІДПРАВИТИ ВІДГУК
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ModalReview;
-
-
-
-
-
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setReviews } from '../../redux/actions';
-import { addReviewApi, fetchReviewsApi } from '../../api/api'; // Проверьте правильность пути
-import icons from './icons.svg'; // Проверьте, что путь до файла с иконками верный
+import { addReviewApi, fetchReviewsApi } from '../../api/api';
+import icons from './icons.svg';
 import './styles.css';
 
 const ModalReview = ({ closeModal, onSuccess }) => {
@@ -377,19 +36,17 @@ const ModalReview = ({ closeModal, onSuccess }) => {
         await addReviewApi(newReview);
         const reviews = await fetchReviewsApi();
         dispatch(setReviews(reviews));
-        onSuccess(); // Предположим, что это функция для уведомления пользователя об успехе
-        closeModal(); // Закрытие модального окна
+        onSuccess();
+        closeModal();
       } catch (error) {
         console.error('Ошибка при добавлении отзыва:', error);
       }
     }
   };
 
-  console.log('Rendering ModalReview...');
-
   return (
-    <div className="modal_overlay" onClick={closeModal}>
-      <div className="modal_content" onClick={e => e.stopPropagation()}>
+    <div className="modale_overlay" onClick={closeModal}>
+      <div className="modale_content" onClick={e => e.stopPropagation()}>
         <button className="close_modal" onClick={closeModal}>
           <svg className="icon_modal">
             <use xlinkHref={`${icons}#close`} />
@@ -397,12 +54,13 @@ const ModalReview = ({ closeModal, onSuccess }) => {
         </button>
         <form className="reviews_form" onSubmit={handleSubmit}>
           <label className="reviews_label">
-            Имя:
+            Ім'я:
             <input
               className="reviews_input"
               type="text"
               name="name"
               value={newReview.name}
+              autoComplete="name"
               onChange={handleInputChange}
             />
             {formErrors.name && (
@@ -410,12 +68,13 @@ const ModalReview = ({ closeModal, onSuccess }) => {
             )}
           </label>
           <br />
-          <label className="reviews_label">
-            Комментарий:
+          <label>
+            Коментар:
             <textarea
               className="reviews_textarea"
               name="comment"
               value={newReview.comment}
+              autoComplete="comment"
               onChange={handleInputChange}
             />
             {formErrors.comment && (
@@ -423,8 +82,9 @@ const ModalReview = ({ closeModal, onSuccess }) => {
             )}
           </label>
           <br />
-          <button className="reviews_button" type="submit">
-            Отправить отзыв
+          <button
+            className="reviews_button" type="submit">
+            ВІДПРАВИТИ ВІДГУК
           </button>
         </form>
       </div>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,112 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setReviews } from '../../redux/actions';
-// import Notiflix from 'notiflix';
-// import { fetchReviewsApi } from '../../api/api';
-// import ReviewsSlider from './ReviewsSlider';
-// import ModalReview from '../../components/Modal/ModalReview';
-// import './styles.css';
-
-// const Reviews = () => {
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const dispatch = useDispatch();
-//   const reviews = useSelector(state => state.reviews);
-
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   useEffect(() => {
-//     const maxRetries = 3;
-//     let retries = 0;
-
-//     const fetchReviews = async () => {
-//       setLoading(true);
-//       try {
-//         const data = await fetchReviewsApi();
-//         dispatch(setReviews(data));
-//         setLoading(false);
-
-//       } catch (error) {
-//         console.error('Ошибка при получении отзывов:', error);
-//         setError(error.message);
-//         setLoading(false);
-
-//         if (retries < maxRetries) {
-//           retries++;
-//           console.log(`Повтор (${retries}/${maxRetries})...`);
-//           fetchReviews();
-//         } else {
-//           console.error(
-//             'Достигнуто максимальное количество повторов. Невозможно получить отзывы.'
-//           );
-//           handleApiError(error);
-//         }
-//       }
-//     };
-
-//     fetchReviews();
-//   }, [dispatch]);
-
-//   const handleApiError = error => {
-//     if (error.response) {
-//       console.error('Данные ответа:', error.response.data);
-//       console.error('Статус ответа:', error.response.status);
-//       console.error('Заголовки ответа:', error.response.headers);
-//     } else if (error.request) {
-//       console.error('Не получен ответ:', error.request);
-//     } else {
-//       console.error('Ошибка настройки запроса:', error.message);
-//       if (error.message.includes('empty fields')) {
-//         Notiflix.Notify.failure('Пожалуйста, заполните все поля формы.');
-//       }
-//     }
-//   };
-
-//   const addReviewSuccess = () => {
-//     Notiflix.Notify.success('Отзыв успешно добавлен!',
-//       {
-//         position: 'center-center'
-//       });
-//   };
-
-//   return (
-//     <div className="reviews">
-//       <h2 className="reviews_h2">ВІДГУКИ</h2>
-
-//       {loading && <p>Завантаження...</p>}
-//       {error && <p>Error: {error}</p>}
-//       {!loading && !error && (
-//         <>
-//           <div>
-//             <ReviewsSlider reviews={reviews} />
-//           </div>
-//           <button className="reviews_button" type="button" onClick={openModal}>
-//             ДОДАТИ ВІДГУК
-//           </button>
-//           {isModalOpen && (
-//             <ModalReview closeModal={closeModal} onSuccess={addReviewSuccess} />
-//           )}
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Reviews;
-
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setReviews } from '../../redux/actions';
@@ -141,6 +32,7 @@ const Reviews = () => {
         const data = await fetchReviewsApi();
         dispatch(setReviews(data));
         setLoading(false);
+
       } catch (error) {
         console.error('Ошибка при получении отзывов:', error);
         setError(error.message);
@@ -154,6 +46,7 @@ const Reviews = () => {
           console.error(
             'Достигнуто максимальное количество повторов. Невозможно получить отзывы.'
           );
+          handleApiError(error);
         }
       }
     };
@@ -161,10 +54,26 @@ const Reviews = () => {
     fetchReviews();
   }, [dispatch]);
 
+  const handleApiError = error => {
+    if (error.response) {
+      console.error('Данные ответа:', error.response.data);
+      console.error('Статус ответа:', error.response.status);
+      console.error('Заголовки ответа:', error.response.headers);
+    } else if (error.request) {
+      console.error('Не получен ответ:', error.request);
+    } else {
+      console.error('Ошибка настройки запроса:', error.message);
+      if (error.message.includes('empty fields')) {
+        Notiflix.Notify.failure('Пожалуйста, заполните все поля формы.');
+      }
+    }
+  };
+
   const addReviewSuccess = () => {
-    Notiflix.Notify.success('Отзыв успешно добавлен!', {
-      position: 'center-center',
-    });
+    Notiflix.Notify.success('Отзыв успешно добавлен!',
+      {
+        position: 'center-center'
+      });
   };
 
   return (
