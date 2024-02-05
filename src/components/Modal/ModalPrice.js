94060f00ef6b10d55aaf65c144984f245d897eb3
Alekcandrovich <ravara128@gmail.com>
2024-02-05 22:30:52 +0200

    сделал анимацию для модалки прайсов

--- src/components/Modal/ModalPrice.js
+++ src/components/Modal/ModalPrice.js
@@ -46,66 +46,98 @@
 
 
 
+// import React, { useEffect } from 'react';
+// import Modal from 'react-modal';
+// import { Transition } from 'react-transition-group';
+// import icons from './icons.svg';
+// import './styles.css';
 
-import React, { useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import Modal from 'react-modal';
+// const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
+//   useEffect(() => {
+//     if (isOpen) {
+//       document.body.classList.add('modal-open');
+//     } else {
+//       document.body.classList.remove('modal-open');
+//     }
+//   }, [isOpen]);
+
+//   const transitionTimeout = 300; // Замените на ваше значение
+
+//   return (
+//     <Transition in={isOpen} timeout={transitionTimeout}>
+//       {state => (
+//         <Modal
+//           isOpen={isOpen}
+//           onRequestClose={onRequestClose}
+//           contentLabel="Image Modal"
+//           overlayClassName="modal_overlay"
+//           className={`modal_price modal-${state}`}
+//         >
+//           <button onClick={onRequestClose} className="close_modal">
+//             <svg className="icon_modal">
+//               <use xlinkHref={`${icons}#close`} />
+//             </svg>
+//           </button>
+//           <div className="image_container">
+//             <img src={imgSrc} alt="ПРАЙС - ЛИСТ" className="modal_image" />
+//           </div>
+//         </Modal>
+//       )}
+//     </Transition>
+//   );
+// };
+
+// export default ModalPrice;
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useCallback } from 'react';
 import icons from './icons.svg';
-import './styles.css';
+import './styles_.css';
 
 const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
+  const closeModal = useCallback(() => {
+    onRequestClose();
+  }, [onRequestClose]);
+
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add('modal-open');
     } else {
       document.body.classList.remove('modal-open');
     }
-  }, [isOpen]);
 
-  const modalVariants = {
-    hidden: { opacity: 0, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: { duration: 0.5, ease: 'easeInOut' },
-    },
-  };
+    const handleEscape = event => {
+      if (isOpen && event.keyCode === 'Escape') closeModal();
+    };
+
+    document.addEventListener('keydown', handleEscape);
+    return () => document.removeEventListener('keydown', handleEscape);
+  }, [isOpen, closeModal]);
+
+  const overlayClassName = isOpen ? 'modal_overlay active' : 'modal_overlay';
+  const modalClassName = isOpen ? 'modal_price active' : 'modal_price';
 
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          key="modal"
-          initial="hidden"
-          animate="visible"
-          exit="hidden"
-          variants={modalVariants}
-        >
-          <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            contentLabel="Image Modal"
-            overlayClassName="modal_overlay"
-            className="modal_price"
-          >
-            <motion.div
-              className="image_container"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.5, ease: 'easeInOut' }}
-            >
-              <button onClick={onRequestClose} className="close_modal">
-                <svg className="icon_modal">
-                  <use xlinkHref={`${icons}#close`} />
-                </svg>
-              </button>
-              <img src={imgSrc} alt="ПРАЙС - ЛИСТ" className="modal_image" />
-            </motion.div>
-          </Modal>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className={overlayClassName} id="modalOverlay" onClick={closeModal}>
+      <div className={modalClassName} onClick={e => e.stopPropagation()}>
+        <button onClick={closeModal} className="close_modal">
+          <svg className="icon_modal">
+            <use xlinkHref={`${icons}#close`} />
+          </svg>
+        </button>
+        <div className="image_container">
+          <img src={imgSrc} alt="ПРАЙС - ЛИСТ" className="modal_image" />
+        </div>
+      </div>
+    </div>
   );
 };
 
