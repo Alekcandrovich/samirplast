58c7a85c0da69bce8ecd5b812680f90d424e8b20
Alekcandrovich <ravara128@gmail.com>
2024-02-05 22:54:29 +0200

    подчистил код и стили для модалки прайсов

--- src/components/Modal/ModalPrice.js
+++ src/components/Modal/ModalPrice.js
@@ -1,106 +1,6 @@
-// import React, { useEffect } from 'react';
-// import Modal from 'react-modal';
-// import { Transition } from 'react-transition-group';
-// import icons from './icons.svg';
-// import './styles.css';
-
-// const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
-//   useEffect(() => {
-//     if (isOpen) {
-//       document.body.classList.add('modal-open');
-//     } else {
-//       document.body.classList.remove('modal-open');
-//     }
-//   }, [isOpen]);
-
-//   return (
-//     <Transition in={isOpen}>
-//       {state => (
-//         <Modal
-//           isOpen={isOpen}
-//           onRequestClose={onRequestClose}
-//           contentLabel="Image Modal"
-//           overlayClassName="modal_overlay"
-//           className={`modal_price modal-${state}`}
-//         >
-//           <button onClick={onRequestClose} className="close_modal">
-//             <svg className="icon_modal">
-//               <use xlinkHref={`${icons}#close`} />
-//             </svg>
-//           </button>
-//           <div className="image_container">
-//             <img src={imgSrc} alt="ПРАЙС - ЛИСТ" className="modal_image" />
-//           </div>
-//         </Modal>
-//       )}
-//     </Transition>
-//   );
-// };
-
-// export default ModalPrice;
-
-
-
-
-
-
-
-
-// import React, { useEffect } from 'react';
-// import Modal from 'react-modal';
-// import { Transition } from 'react-transition-group';
-// import icons from './icons.svg';
-// import './styles.css';
-
-// const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
-//   useEffect(() => {
-//     if (isOpen) {
-//       document.body.classList.add('modal-open');
-//     } else {
-//       document.body.classList.remove('modal-open');
-//     }
-//   }, [isOpen]);
-
-//   const transitionTimeout = 300; // Замените на ваше значение
-
-//   return (
-//     <Transition in={isOpen} timeout={transitionTimeout}>
-//       {state => (
-//         <Modal
-//           isOpen={isOpen}
-//           onRequestClose={onRequestClose}
-//           contentLabel="Image Modal"
-//           overlayClassName="modal_overlay"
-//           className={`modal_price modal-${state}`}
-//         >
-//           <button onClick={onRequestClose} className="close_modal">
-//             <svg className="icon_modal">
-//               <use xlinkHref={`${icons}#close`} />
-//             </svg>
-//           </button>
-//           <div className="image_container">
-//             <img src={imgSrc} alt="ПРАЙС - ЛИСТ" className="modal_image" />
-//           </div>
-//         </Modal>
-//       )}
-//     </Transition>
-//   );
-// };
-
-// export default ModalPrice;
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect, useCallback } from 'react';
 import icons from './icons.svg';
-import './styles_.css';
+import './styles.css';
 
 const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
   const closeModal = useCallback(() => {
@@ -110,16 +10,21 @@ const ModalPrice = ({ imgSrc, isOpen, onRequestClose }) => {
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add('modal-open');
+      document.body.style.overflow = 'hidden';
     } else {
       document.body.classList.remove('modal-open');
+      document.body.style.overflow = 'auto';
     }
 
     const handleEscape = event => {
-      if (isOpen && event.keyCode === 'Escape') closeModal();
+      if (isOpen && event.key === 'Escape') closeModal();
     };
 
     document.addEventListener('keydown', handleEscape);
-    return () => document.removeEventListener('keydown', handleEscape);
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'auto';
+    };
   }, [isOpen, closeModal]);
 
   const overlayClassName = isOpen ? 'modal_overlay active' : 'modal_overlay';
