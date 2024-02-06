19aeba9d9824772eb68925d686941f7ff4b6a928
Alekcandrovich <ravara128@gmail.com>
2024-02-06 17:03:23 +0200

    удалил лишние библиотеки

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,12 +1,8 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Modal from 'react-modal';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-  const rootElement = document.getElementById('root');
-
-  Modal.setAppElement(rootElement);
 
   useEffect(() => {
     window.scrollTo(0, 0);
