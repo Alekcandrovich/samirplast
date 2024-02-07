00f676e6fec06a3450e29c868f0a153e40c9e79e
Alekcandrovich <ravara128@gmail.com>
2024-02-08 01:06:24 +0200

    разработка анимации подсказки мышки при наведении на объект

--- src/components/ProductsSet/ProductDetailSection.js
+++ src/components/ProductsSet/ProductDetailSection.js
@@ -1,4 +1,40 @@
-import React from 'react';
+// import React from 'react';
+
+// const ProductDetailSection = ({
+//   productImage,
+//   title,
+//   description,
+//   openSlider,
+//   sliderImages,
+// }) => {
+//   return (
+//     <section className="section3" onClick={() => openSlider(sliderImages)}>
+//       <div className="container">
+//         <div className="product_detail open-modal_product">
+//           <img
+//             className="product_detail_image"
+//             src={productImage}
+//             alt={title}
+//           />
+//           <div className="product_detail_info">
+//             <h2>{title}</h2>
+//             <p dangerouslySetInnerHTML={{ __html: description }} />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default ProductDetailSection;
+
+
+
+
+
+
+
+import React, { useState } from 'react';
 
 const ProductDetailSection = ({
   productImage,
@@ -7,10 +43,34 @@ const ProductDetailSection = ({
   openSlider,
   sliderImages,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseEnter = e => {
+    setIsHovered(true);
+    setTooltipPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <section className="section3" onClick={() => openSlider(sliderImages)}>
+    <section
+      className="section3"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="container">
         <div className="product_detail open-modal_product">
+          {isHovered && (
+            <div
+              className="hover-message"
+              style={{ top: tooltipPosition.y, left: tooltipPosition.x }}
+            >
+              Нажми для получения больше информации о продукте
+            </div>
+          )}
           <img
             className="product_detail_image"
             src={productImage}
